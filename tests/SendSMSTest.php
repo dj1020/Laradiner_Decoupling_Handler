3eeedc62113d101238b11ffc59ff13e604a01d3f
Ken Lin <ken.lin@cloud-interactive.com>
2015-11-26 07:23:16 +0800

    (綠燈)整個 test case 改寫，因為不再是透過 event handler 發簡訊了

--- tests/SendSMSTest.php
+++ tests/SendSMSTest.php
@@ -28,38 +28,27 @@ class SendSMSTest extends TestCase
     {
         // Arrange
         $data = [
-            'phone' => '0988123456',
+            'phone'   => '0988123456',
             'message' => 'my test message here',
-            'user'  => [
-                'id' => 1
-            ]
         ];
 
-        // 挑戰 2：在修改最少的情況下，讓這個 Mitake_SMS 類別可以被 Mock 取代，進而測試 handle 方法。
-        // Solution 1:
-        $fakeEvent = \Mockery::mock(SendSMSEvent::class);
-        $fakeCourier = \Mockery::mock(SmsCourierInterface::class);
+        $user = \Mockery::mock('App\User[sms]');
+        $courier = \Mockery::mock(SmsCourierInterface::class);
+        $relation = \Mockery::mock('stdClass');
 
-        $fakeEvent->shouldReceive('getData')->once()->andReturn($data);
-        $fakeCourier->shouldReceive('sendTextMessage')->once()->with([
+        $courier->shouldReceive('sendTextMessage')->once()->with([
             'to'      => $data['phone'],
             'message' => $data['message']
         ]);
 
-        // 挑戰 3：如何在不觸及資料庫操作的前提下，寫測試驗證 handle 方法內的處理邏輯？
-        // Solution 1:
-        $users = \Mockery::mock(User::class);
-        $fakeUser = \Mockery::mock(User::class);
-        $relation = \Mockery::mock('stdClass');
+        $user->shouldReceive('sms')->once()->andReturn($relation);
 
-        $users->shouldReceive('find')->once()->andReturn($fakeUser);
-        $fakeUser->shouldReceive('messages')->once()->andReturn($relation);
         $relation->shouldReceive('create')->once()->with([
             'to'      => $data['phone'],
             'message' => $data['message']
         ]);
 
         // Act & Assert
-        (new SendSMS($users, $fakeCourier))->handle($fakeEvent);
+        $user->sendSms($courier, $data['phone'], $data['message']);
     }
 }
