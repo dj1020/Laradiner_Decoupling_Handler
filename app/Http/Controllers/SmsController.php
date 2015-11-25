9439d6ab7fa7a95db05f9e9c8a5e2935d683d5ad
Ken Lin <ken.lin@cloud-interactive.com>
2015-11-26 07:40:24 +0800

    記得提取 Interface 後要做綁定，Laravel 才知道去使用哪一個實作的 class

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Events\SendSMSEvent;
 use App\Sms\Mitake_SMS;
+use App\Sms\SmsCourierInterface;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\App;
 
 class SmsController extends Controller
 {
@@ -31,6 +33,8 @@ class SmsController extends Controller
     {
         $data = $request->except("_token");
         $data['user']['id'] = 1;
+
+        App::instance(SmsCourierInterface::class, new Mitake_SMS($this->apiKey));
 
         event(new SendSMSEvent($data));
     }
