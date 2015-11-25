d885c9a3e87e4c3294c0c8d74104717c445b4a5e
Ken Lin <ken.lin@cloud-interactive.com>
2015-11-26 06:27:22 +0800

    重構 test case

--- tests/SendSMSTest.php
+++ tests/SendSMSTest.php
@@ -27,24 +27,25 @@ class SendSMSTest extends TestCase
     public function it_should_send_sms()
     {
         // Arrange
+        $data = [
+            'phone' => '0988123456',
+            'message' => 'my test message here',
+            'user'  => [
+                'id' => 1
+            ]
+        ];
 
         // 挑戰 2：在修改最少的情況下，讓這個 Mitake_SMS 類別可以被 Mock 取代，進而測試 handle 方法。
         // Solution 1:
-        $fakeCourier = \Mockery::mock(Mitake_SMS::class);
-        $fakeCourier->shouldReceive('sendTextMessage')->once()->with([
-            'to'      => '0988123456',
-            'message' => 'my test message here'
-        ]);
-
         $fakeEvent = \Mockery::mock(SendSMSEvent::class);
-        $fakeEvent->shouldReceive('getData')->once()->andReturn([
-            'phone'   => '0988123456',
-            'message' => 'my test message here',
-            'user'    => [
-                'id' => 1
-            ]
-        ]);
+        $fakeCourier = \Mockery::mock(Mitake_SMS::class);
+
+        $fakeEvent->shouldReceive('getData')->once()->andReturn($data);
         $fakeEvent->shouldReceive('getCourier')->once()->andReturn($fakeCourier);
+        $fakeCourier->shouldReceive('sendTextMessage')->once()->with([
+            'to'      => $data['phone'],
+            'message' => $data['message']
+        ]);
 
         // 挑戰 3：如何在不觸及資料庫操作的前提下，寫測試驗證 handle 方法內的處理邏輯？
         // Solution 1:
@@ -55,13 +56,11 @@ class SendSMSTest extends TestCase
         $users->shouldReceive('find')->once()->andReturn($fakeUser);
         $fakeUser->shouldReceive('messages')->once()->andReturn($relation);
         $relation->shouldReceive('create')->once()->with([
-            'to'      => '0988123456',
-            'message' => 'my test message here',
+            'to'      => $data['phone'],
+            'message' => $data['message']
         ]);
 
-        $handler = new SendSMS($users);
-
         // Act & Assert
-        $handler->handle($fakeEvent);
+        (new SendSMS($users))->handle($fakeEvent);
     }
 }
