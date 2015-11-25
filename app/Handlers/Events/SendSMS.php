c2c0d3684cf922978484b1d97432869554a00e3e
Ken Lin <ken.lin@cloud-interactive.com>
2015-11-26 04:09:21 +0800

    建立 event(SendSMSEvent class) 和 event handler(SendSMS class)

---
+++ app/Handlers/Events/SendSMS.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Handlers\Events;
+
+use App\Events\SendSMSEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendSMS implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * Create the event handler.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  SendSMSEvent  $event
+     * @return void
+     */
+    public function handle(SendSMSEvent $event)
+    {
+        //
+    }
+}
