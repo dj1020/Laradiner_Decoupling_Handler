c93c1d37ebc97f26b08cd4bd68080d65ac542e7f
Ken Lin <ken.lin@cloud-interactive.com>
2015-11-26 05:17:05 +0800

    建立 User 和 Message 之間的 relation，什麼！？ MassAssignmentException

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function messages()
+    {
+        return $this->hasMany(Messages::class);
+    }
 }
